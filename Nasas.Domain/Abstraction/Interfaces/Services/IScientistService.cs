0372d461b7305b004281bc6233de8c38c863f5e8
Mazur-Stas <mazur.stas5400@gmail.com>
2025-06-30 20:20:07 +0300

    Create new Interface and Dto

---
+++ Nasas.Domain/Abstraction/Interfaces/Services/IScientistService.cs
@@ -0,0 +1,16 @@
+﻿using Nasas.Domain.Dtos.Input;
+using Nasas.Domain.Models;
+
+namespace Nasas.Domain.Abstraction.Interfaces.Services;
+
+public interface IScientistService
+{
+    Task<Scientist> AddScientistAsync(ScientistDto scientist, CancellationToken cancellationToken);
+
+    Task<IEnumerable<Scientist>> GetAllScientistAsync(CancellationToken cancellationToken);
+
+    Task<Scientist> EditScientistAsync(ScientistDto scientist, CancellationToken cancellationToken);
+
+    Task<bool> DeleteScientistAsync(int id, CancellationToken cancellationToken);
+}
+
---
+++ Nasas.Domain/Dtos/Input/ScientistDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Nasas.Domain.Dtos.Input;
+
+public class ScientistDto
+{
+    public string FirstName { get; set; }
+
+    public string LastName { get; set; }
+
+    public string FullName => $"{LastName} {FirstName}";
+}
+
