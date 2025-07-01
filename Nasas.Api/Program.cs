f183ee781a09c8800613449a56d453fb35a6570d
BXlon_ub54@student.itstep.org <BXlon_ub54@student.itstep.org>
2025-07-01 20:49:49 +0300

    Refactoring code

--- Nasas.Api/Program.cs
+++ Nasas.Api/Program.cs
@@ -1,5 +1,9 @@
+using Microsoft.EntityFrameworkCore;
 using Nasas.Application.Services;
+using Nasas.Domain.Abstraction.Interfaces.Repositories;
 using Nasas.Domain.Abstraction.Interfaces.Services;
+using Nasas.Infrastructure.Data;
+using Nasas.Infrastructure.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,10 +12,15 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-
+builder.Services.AddDbContext<NasasDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddScoped<ILoginService, LoginService>();
+builder.Services.AddScoped<IScientistService, ScientistService>();
 builder.Services.AddScoped<IPlanetService, PlanetService>();
-builder.Services.AddScoped<IPlanetService, PlanetService>();
+builder.Services.AddScoped<ILoginRepository, LoginRepository>();
+builder.Services.AddScoped<IPlanetRepository, PlanetRepository>();
+builder.Services.AddScoped<IScientistRepository, ScientistRepository>();
+
 
 var app = builder.Build();
 
