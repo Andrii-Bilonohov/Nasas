efb78f3f1b7c28c7311e3f5ac4d1863097f4ed73
BXlon_ub54@student.itstep.org <BXlon_ub54@student.itstep.org>
2025-07-01 19:16:15 +0300

    Create services

---
+++ Nasas.Application/Services/LoginService.cs
@@ -0,0 +1,55 @@
+﻿using Nasas.Domain.Abstraction.Interfaces.Repositories;
+using Nasas.Domain.Abstraction.Interfaces.Services;
+using Nasas.Domain.Abstraction.Models;
+using Nasas.Domain.Dtos.Input;
+using Nasas.Domain.Dtos.Output;
+using Nasas.Domain.Models;
+
+namespace Nasas.Application.Services
+{
+    public class LoginService : ILoginService
+    { 
+        private readonly ILoginRepository _loginRepository;
+
+
+        public LoginService(ILoginRepository loginRepository)
+        {
+            _loginRepository = loginRepository ?? throw new ArgumentNullException(nameof(loginRepository));
+        }
+
+
+        public Task<Login> LoginAsync(LoginDto loginDtos, CancellationToken cancellationToken)
+        {
+            if (loginDtos == null)
+            {
+                throw new ArgumentNullException(nameof(loginDtos));
+            }
+
+            return _loginRepository.GetLoginAsync(loginDtos.UserName, loginDtos.Password, cancellationToken);
+        }
+
+        public async Task<Login> RegisterAsync(RegisterDto registerDtos, CancellationToken cancellationToken)
+        {
+            if (registerDtos == null)
+            {
+                throw new ArgumentNullException(nameof(registerDtos));
+            }
+
+            await _loginRepository.IsLoginExistsAsync(new Login
+            {
+                UserName = registerDtos.UserName,
+                Email = registerDtos.Email,
+                Password = registerDtos.Password,
+            }, cancellationToken);
+
+            var newLogin = new Login
+            {
+                UserName = registerDtos.UserName,
+                Email = registerDtos.Email,
+                Password = registerDtos.Password,
+            };
+
+            return await _loginRepository.AddAsync(newLogin, cancellationToken);
+        }
+    }
+}
---
+++ Nasas.Application/Services/ScientlistService.cs
@@ -0,0 +1,68 @@
+﻿using Nasas.Domain.Abstraction.Interfaces.Repositories;
+using Nasas.Domain.Abstraction.Interfaces.Services;
+using Nasas.Domain.Dtos.Input;
+using Nasas.Domain.Models;
+
+namespace Nasas.Application.Services
+{
+    public class ScientlistService : IScientistService
+    {
+        private readonly IScientistRepository _scientistRepository;
+
+
+        public ScientlistService(IScientistRepository scientistRepository)
+        {
+            _scientistRepository = scientistRepository ?? throw new ArgumentNullException(nameof(scientistRepository));
+        }
+
+
+        public Task<Scientist> AddScientistAsync(ScientistDto scientist, CancellationToken cancellationToken)
+        {
+            if (scientist == null)
+            {
+                throw new ArgumentNullException(nameof(scientist));
+            }
+
+            var newScientist = new Scientist
+            { 
+              FirstName = scientist.FirstName, 
+              LastName = scientist.LastName 
+            };
+
+            return _scientistRepository.AddAsync(newScientist, cancellationToken);
+        }
+
+
+        public Task<bool> DeleteScientistAsync(int id, CancellationToken cancellationToken)
+        {
+            if (id <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(id), "ID must be greater than zero.");
+            }
+            return _scientistRepository.DeleteAsync(id, cancellationToken);
+        }
+
+
+        public Task<Scientist> EditScientistAsync(ScientistDto scientist, CancellationToken cancellationToken)
+        {
+            if (scientist == null)
+            {
+                throw new ArgumentNullException(nameof(scientist));
+            }
+
+            var updatedScientist = new Scientist
+            { 
+              FirstName = scientist.FirstName, 
+              LastName = scientist.LastName 
+            };
+
+            return _scientistRepository.UpdateAsync(updatedScientist, cancellationToken);
+        }
+
+
+        public Task<IEnumerable<Scientist>> GetAllScientistAsync(CancellationToken cancellationToken)
+        {
+            return _scientistRepository.GetAllAsync(cancellationToken);
+        }
+    }
+}
