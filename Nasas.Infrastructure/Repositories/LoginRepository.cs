6fcf4a4a22b2aa010ed03a7bdf34ec7396448196
BXlon_ub54@student.itstep.org <BXlon_ub54@student.itstep.org>
2025-07-01 15:30:49 +0300

    Add few repositories and modify planetRepository

---
+++ Nasas.Infrastructure/Repositories/LoginRepository.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Nasas.Domain.Abstraction.Interfaces.Repositories;
+using Nasas.Domain.Models;
+using Nasas.Infrastructure.Data;
+
+namespace Nasas.Infrastructure.Repositories
+{
+    public class LoginRepository : ILoginRepository
+    {
+        private readonly NasasDbContext _context;
+
+
+        public LoginRepository(NasasDbContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+
+        public async Task<Login> AddAsync(Login login, CancellationToken cancellationToken)
+        {
+            _context.Logins.Add(login);
+            await _context.SaveChangesAsync(cancellationToken);
+            return login;
+        }
+
+
+        public async Task<bool> DeleteAsync(int id, CancellationToken cancellationToken)
+        {
+            var login = await _context.Logins.FindAsync(new object[] { id }, cancellationToken);
+            
+            _context.Logins.Remove(login);
+            await _context.SaveChangesAsync(cancellationToken);
+            return true;
+        }
+
+
+        public async Task<IEnumerable<Login>> GetAllAsync(CancellationToken cancellationToken)
+        {
+            return await _context.Logins.ToListAsync(cancellationToken);
+        }
+
+
+        public async Task<Login?> GetLoginAsync(string username, string password, CancellationToken cancellationToken)
+        {
+            return await _context.Logins
+                .Include(l => l.User)
+                .FirstOrDefaultAsync(l => l.UserName == username && l.Password == password, cancellationToken);
+        }
+
+
+        public async Task<Login> UpdateAsync(Login login, CancellationToken cancellationToken)
+        {
+            _context.Logins.Update(login);
+            await _context.SaveChangesAsync(cancellationToken);
+            return login;
+        }
+    }
+}
---
+++ Nasas.Infrastructure/Repositories/ScientistRepository.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Nasas.Domain.Abstraction.Interfaces.Repositories;
+using Nasas.Domain.Models;
+using Nasas.Infrastructure.Data;
+
+namespace Nasas.Infrastructure.Repositories
+{
+    public class ScientistRepository : IScientistRepository
+    {
+        private readonly NasasDbContext _context;
+
+
+        public ScientistRepository(NasasDbContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+
+        public async Task<Scientist> AddAsync(Scientist scientist, CancellationToken cancellationToken)
+        {
+            _context.Scientists.Add(scientist);
+            await _context.SaveChangesAsync(cancellationToken);
+            return scientist;
+        }
+
+
+        public async Task<bool> DeleteAsync(int id, CancellationToken cancellationToken)
+        {
+            var scientist = await _context.Scientists.FindAsync(new object[] { id }, cancellationToken);
+            _context.Scientists.Remove(scientist);
+            await _context.SaveChangesAsync(cancellationToken);
+            return true;
+        }
+        
+
+        public async Task<IEnumerable<Scientist>> GetAllAsync(CancellationToken cancellationToken)
+        {
+            return await _context.Scientists.Include(s => s.Planets)
+                .Include(s => s.FullName)
+                .ToListAsync(cancellationToken);
+        }
+
+
+        public async Task<Scientist> UpdateAsync(Scientist scientist, CancellationToken cancellationToken)
+        {
+            _context.Scientists.Update(scientist);
+            await _context.SaveChangesAsync(cancellationToken);
+            return scientist;
+        }
+    }
+}
